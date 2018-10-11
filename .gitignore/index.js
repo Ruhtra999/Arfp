1505032844f1023dca43d026f3bcb24ba8fe83ad
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2018-10-11 11:09:42 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
-bot.login(process.env.TOKEN)
-
 var prefix = "j/";
 
 client.user.setPresence({game: {name: `<help |by Ruhtra |serveurs : ${client.guilds.size}`}})
@@ -106,3 +104,5 @@ client.on('message', message => {
     }
 
 });
+
+bot.login(process.env.TOKEN)
