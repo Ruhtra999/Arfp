c4425d0aca0e761be18086abbbf434d3a705fdb6
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2018-10-11 18:45:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -26,7 +26,7 @@ if(message.content === prefix + "help"){
 }
  
  case "serverlist";
-message.channel.send(bot.guild.map(r => r.name + ` | **${r.memberCount}** membres`))
+message.channel.send(bot.guilds.map(r => r.name + ` | **${r.memberCount}** membres`))
        
 if(message.content.startsWith(prefix + "kick")){
   if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.channel.send("Vous n'avez pas la permission pour kick !");
