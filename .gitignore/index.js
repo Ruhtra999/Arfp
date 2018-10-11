2f456237c618126b4ce2a4494403a7d2faea2218
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2018-10-11 11:16:36 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,20 +1,9 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const Discord = require('discord.js')
+const bot = new Discord.Client()
 
-bot.login(process.env.TOKEN);
+bot.login(process.env.TOKEN)
 
 var prefix = "j/";
 
 client.user.setPresence({game: {name: `<help |by Ruhtra |serveurs : ${client.guilds.size}`}})
 });
-
-client.on('message', message => {
-
-    if(message.content === "bonjour"){
-        message.reply("Salut");
-        console.log('Le bot dit salut');    
-    }
-    if(message.content === "Bonjour"){
-        message.reply("Salut");
-        console.log('Le bot dit salut');
-    }
