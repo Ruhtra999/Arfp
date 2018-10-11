eff307794cd5baf56fc0a9f6088e0d0bab695450
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2018-10-11 13:30:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -7,4 +7,11 @@ bot.on('ready', function () {
   bot.user.setGame('j/help').catch(console.error)
 })
 
+bot.on('message', message => {
+
+  if(message.content === "bonjour"){
+    message.channel.send("Bonjour").catch(console.error);
+  }
+});
+
 bot.login(process.env.TOKEN)
