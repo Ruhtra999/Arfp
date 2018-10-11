11c773408bafd38764c89c4364b8ec8397550c24
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2018-10-11 13:47:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,9 +9,19 @@ bot.on('ready', function () {
 
 bot.on('message', message => {
 
-  if(message.content === "bonjour"){
+  if(message.content === "Bonjour"){
     message.channel.send("Bonjour").catch(console.error);
   }
+  
+if(message.content === prefix + "help"){
+  var help_embed = new Discord.RichEmbed()
+  .setColor("#0000CC")
+  .setTitle("Commandes du bot :")
+  .setDescription("Prefix du bot : j/")
+  .addField("help", "L'endroit ou vous êtes maintenant ^^")
+  .setFooter("Prefix du bot : j/")
+  message.channel.sendMessage(help_embed).catch(console.error);
+}
 });
 
 bot.login(process.env.TOKEN)
