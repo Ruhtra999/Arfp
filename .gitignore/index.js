74d53733bf55b197b0c12c31a1ec0e9c7488fb51
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2018-12-23 22:28:44 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,4 +3,12 @@ const bot = new Discord.Client();
 
 var prefix = ("&?")
 
+bot.on('message', async message => { 
+
+    if(message.content === "Arfp"){
+        message.reply("Oui?");
+    }
+
+});
+
 bot.login(process.env.TOKEN);
