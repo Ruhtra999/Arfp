a41ec395ee768950eb8d4ab53f5d4ad67cea8905
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2018-12-23 20:50:52 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,39 +12,5 @@ bot.on('message', message => {
   if(message.content === "Bonjour"){
     message.channel.send("Salut").catch(console.error);
   }
-  
-if(message.content === prefix + "help"){
-  var help_embed = new Discord.RichEmbed()
-  .setColor("#0000CC")
-  .setTitle("Commandes du bot :")
-  .setDescription("Prefix du bot : j/")
-  .addField("j/help", "L'endroit ou vous êtes maintenant ^^")
-  .addField("j/kick", "Pour ban un utilisateur mais il peux rerejoindre directement.")
-  .addField("j/serverlist", "Fait apparaître la liste des serveurs où est le bot et le nombre de membres du serveur")
-  .setFooter("Prefix du bot : j/")
-  message.channel.sendMessage(help_embed).catch(console.error);
-}
- 
-if(message.content.startsWith(prefix + "kick")){
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.channel.send("Vous n'avez pas la permission pour kick !");
-}
-
-  if(message.mentions.users.size === 0) {
-      return message.channel.send("L'utilisateur na pas été mentionner !")  }
-  var kick = message.guild.member(message.mentions.users.first());
-  if(!kick) {
-      return message.channel.send("L'utilisateur a l'air invalide !")
-  }
-
-  if(message.guild.member(client.user).hasPermission("KICK_MEMBERS")) {
-      return message.channel.send("Donner moi là permision pour kick !");
-  }
-
-  kick.kick().then(member => {
-      message.channel.send(`${member.user.username} est kick pas ${message.author.username}`);
-  });
-}
-  
-});
 
 bot.login(process.env.TOKEN)
