5109b06f357fabdea073549dada905ff9d7d8cbe
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2018-12-25 23:02:10 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -320,6 +320,17 @@ if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return m
  
   }
     
+          if(!message.member.hasPermission("ADMINISTRATOR")) return;
+      if(args[0] == "help"){
+        message.reply("Usage: !say <message>");
+        return;
+      }
+      const sayMessage = args.join(" ");
+      message.delete().catch();
+      message.channel.send(sayMessage);
+
+}
+    
 });
 
 bot.login(process.env.TOKEN);
