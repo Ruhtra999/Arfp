83cbe644d8e3e52068026cd9974ddf2d4b20c6e7
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2018-12-27 17:02:02 +0100

    Update say.js

--- .gitignore/say.js
+++ .gitignore/say.js
@@ -1,14 +1,1 @@
-const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
-
-      if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Non.");
-      const botmessage = args.join(" ");
-      message.delete().catch();
-      message.channel.send(botmessage);
-
-}
-
-module.exports.help = {
-  name: "say"
-}
