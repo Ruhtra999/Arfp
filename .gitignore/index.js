bf7d1efae11c139069c14bec182ca0828dec8ba0
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2019-01-31 19:46:02 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,9 +10,9 @@ bot.on('ready', async () => {
 });
 
 bot.on('message', message => {
-  if(message.content === prefix + "ping"){
-     message.channel.sendMessage("pong !");
-  }
+    if (message.content === prefix + "ping"){
+       message.channel.sendMessage("pong !");
+    }
   
 });
 
