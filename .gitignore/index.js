8b221c80cf380dd4607f1a770d4c2940ec07fc79
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2019-02-04 12:26:26 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -15,12 +15,14 @@ bot.on('ready', async () => {
    if (message.content === prefix + "ping"){
        message.channel.sendMessage('pong ! `' + `${message.createdTimestamp - Date.now()}` + 'ms`');
    }
-   
+   if (message.conntent === prefix + "clear"){
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Non.");
   if(!args[0]) return message.channel.send("non");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
 
+}
+                                           
 }
   
 });
