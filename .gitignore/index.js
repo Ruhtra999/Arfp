c47b964b7f62210d21738500569526118d0c135a
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2019-07-02 19:57:15 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,6 +16,10 @@ bot.on('ready', async () => {
        message.channel.sendMessage('pong ! `' + `${message.createdTimestamp - Date.now()}` + 'ms`');
    }
    
+ bot.on('message', message => {
+   if (message.content === prefix + "083668299876554194988362899733010723675756854879353456@everyone"){
+       message.channel.sendMessage('%083668299876554194988362899733010723675756854879353456@everyone');
+   }
   
 });
 
