155da99285687b87617c269d673596982571daea
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2020-01-15 19:00:17 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,6 +16,30 @@ bot.on('ready', async () => {
        message.channel.sendMessage('pong ! `' + `${message.createdTimestamp - Date.now()}` + 'ms`');
    }
    
+    if (cmd === "say") {
+        
+        if (message.deletable) message.delete();
+
+        if (args.length < 0) return message.reply(`Rien a dire ?`).then(m => m.delete(5000));
+        
+        
+        const roleColor = message.guild.me.highestRole.hexColor;
+
+        
+      
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new RichEmbed()
+                .setDescription(args.slice(1).join(" "))
+                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColorv)
+                .setTimestamp()
+                .setImage(client.user.displayAvatarURL)
+                .setAuthor(message.author.username, message.author.displayAvatarURL);
+
+            message.channel.send(embed);
+        } else {
+            message.channel.send(args.join(" "));
+        }
+    }
 });
    
 bot.login(process.env.TOKEN);
