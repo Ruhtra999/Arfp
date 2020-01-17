ebaeb205a1c702654e9eb08ccc154e33b1633ee7
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2020-01-17 11:22:06 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,3 +2,16 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 client.login(process.env.TOKEN)
+
+
+
+client.on ('message', message => {
+  var prefix = '++'
+  
+  if(message.content.startWith(`${prefix}say`)) {
+    var text = message.content.split(' ').slice(1).join(' ')
+    if(!text) return message.reply('Dis quelque choses!')
+   message.channel.send(text)
+  }
+  
+  })
