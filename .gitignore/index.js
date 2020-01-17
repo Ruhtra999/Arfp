b157b14974ddcf50f0e60cf4c85a7a05583132ae
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2020-01-17 11:43:16 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,17 +1,23 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-client.login(process.env.TOKEN)
+
+
+client.on("ready", () => {
+    client.user.setPresence({ game: { name: `ì̸̠̙̗̻̫̯͇̽͆̃̀̃̐̆̋͐͑́̔̚̚͝n̷̢̨̡̡̢̯̘̲̙̳̳̰͚̜̬͔̹̣͙̲̳̫̪̗̣͔̙̂̈́͐̃̽̉̾͌̉̌͋̆͜͝ ̸̨̢̢̢̛͔͇̺͍͈̞͔̤̦̳̘̙̙̭͇̪̙͔͇̤̭͍͉̺͈̤͇́͆͊̔̓̃̉̋͌͆́̎́̑̊̃͑̀͝ͅͅͅẄ̸̨̧̗͚̤̮̙͈̭̩̫̼͕̥̜̣̩̱͔̪͙̖͍́̅͗̔̏͆͑͒̃̿̈́͊̂̏͗͋͊͌͋́̀̅͘͘̚ơ̵̧̨̰͍̺̱̙͑̄̈̈͐̀͊̀̓̊̋̃͑͒͂̑̇́̓̎͛̇̚͝r̵̡͙̞͈̼̮̾͛̀̽̔̈́̏̍͋͑̇̑̉̊́̐͗͋͑͌͑̏͘͠ķ̸̨̩̯̭̝̫̳̹̱͈̩̖̩̣̊̾̂̎̋͆͗͑̾̽̅͌͂̋̇͊̓̂̆͂̊̈́̉̍̊̀̔̽̌͒̂͘͝͝`, type: 0} });
+
 
 
 
 client.on ('message', message => {
-  var prefix = 'tp!'
+  var prefix = '!'
   
   if(message.content.startWith(`${prefix}say`)) {
-    var text = message.content.split(' ').slice(1).join(' ')
-    if(!text) return message.reply('Dis quelque choses!')
-   message.channel.send(text)
+    var text = message.content.split(' ').slice(1).join(' ');
+    if(!text) return message.reply('Dis quelque choses!');
+   message.channel.send(text);
   }
   
-  })
+  });
+  
+  client.login(process.env.TOKEN);
