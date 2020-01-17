fc9839aecbef33cc395aee5ee74ddadc889a17a4
Ruhtra999 <44002784+Ruhtra999@users.noreply.github.com>
2020-01-17 11:27:14 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,7 +9,6 @@ client.on ('message', message => {
   var prefix = 'tp!'
   
   if(message.content.startWith(`${prefix}say`)) {
-    message.delete()
     var text = message.content.split(' ').slice(1).join(' ')
     if(!text) return message.reply('Dis quelque choses!')
    message.channel.send(text)
